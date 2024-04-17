728ef2dadefd2e3a9a3af69103a4cfd8fef6304c
Axel Person <axel.emil.person@outlook.com>
2024-04-17 12:13:46 +0200

    Cleanup

--- ex-js/src/main.js
+++
@@ -1,7 +0,0 @@
-function toggleContent() {
-  const searchContent = document.getElementById("searchContent");
-  const elementsToToggle = searchContent.querySelectorAll(".hidden"); // Select all elements with the class "hidden" inside the nav
-  elementsToToggle.forEach((element) => {
-    element.classList.toggle("hidden");
-  });
-}
